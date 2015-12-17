a637149b4932a01bb0ddf5737dec82b892aee74e
Ubuntu <xjr30226@126.com>
2015-12-17 21:46:58 +0800

    edit

--- phpspider.php
+++ phpspider.php
@@ -12,33 +12,10 @@
 	*
 	**************************/
 
-	include "Snoopy/Snoopy.class.php";
-	$snoopy = new Snoopy();	
+	$pattern = "/data-tools='{\"title\":\"(.*)\",\"url\":\"(.*)\"}/";
+    $url = "./php.html";
+	$html = file_get_contents($url);
 	
-	class SnoopySpider {
-		
-		function spider($keyword) {
-			// 函数外的变量不能在函数内直接使用，虽然其为全局变量
-			// 需要将其显式地转换为全局变量后，方可使用
-			global $snoopy;
-			$url = "http://www.baidu.com/s?wd=".$keyword;
-			$snoopy -> fetchtext($url);
-			$r = $snoopy -> results;
-			$this -> show($r);
-		}
-		
-		function show($v) {
-			if (is_array($v)) {
-				echo "<pre>";
-				print_r($v);
-				echo "</pre>";
-			} else {
-				echo $v;
-			}
-		}
-	}
-
-	$test = new SnoopySpider();
-	$test -> spider("php");
-
+	preg_match_all($pattern, $html, $matches);
+	print_r($matches);
 ?>
