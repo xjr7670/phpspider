0c98d5ad82320fd7f57da39829ccc5d42723b69d
Cavin <xjr30226@126.com>
2016-01-11 10:10:05 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -7,7 +7,7 @@ import urllib
 import urllib2
 url = "http://www.xjr7670.com/admin/login.php?referer=http://www.xjr7670.com/admin/"
 user_agent = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0"
-values = {'name':'admin', 'password':'x45668668'}
+values = {'name':'', 'password':''}
 headers = {'User-Agent': user_agent, 'Referer': 'http://www.xjr7670.com/articles'}
 data = urllib.urlencode(values)
 request = urllib2.Request(url, data, headers)
